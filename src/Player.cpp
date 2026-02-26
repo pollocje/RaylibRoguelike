5c4b5853189290e7aa1132e3ca3871dfebaad20f
Jeff <jeffrey.pollock123@gmail.com>
2026-02-26 11:22:29 -0500

    Created base Player class/header

---
+++ src/Player.cpp
@@ -0,0 +1,8 @@
+#include "Player.h"
+
+// Implement Constructor
+Player::Player() {
+  health = 100;
+  mana = 100;
+  carryWeight = 0;
+};
---
+++ src/Player.h
@@ -0,0 +1,18 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "raylib.h"
+
+class Player {
+private:
+  // Basic Stats
+  int health;
+  int mana;
+  int carryWeight;
+
+public:
+  // Constructor
+  Player();
+};
+
+#endif
