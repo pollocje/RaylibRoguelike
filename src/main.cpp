3942c7aaa35ccf36a6cd4f80984330985a296fec
Jeff <jeffrey.pollock123@gmail.com>
2026-02-25 21:24:20 -0500

    Added grid

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,42 @@
 const int screenWidth = 800;
 const int screenHeight = 450;
 
+// ----- Function Declarations -----
+
+// Draw Grid Function (Eventually Removed)
+void drawGrid(int width, int height, int cellW, int cellH);
+
 int main() {
 
   InitWindow(screenWidth, screenHeight, "semGame");
   SetTargetFPS(30);
 
   while (!WindowShouldClose()) {
+
     BeginDrawing();
 
     ClearBackground(BLACK);
-    DrawText("semGame is running!", 190, 200, 20, GREEN);
+    drawGrid(screenWidth, screenHeight, 80, 45);
 
     EndDrawing();
   }
 
   CloseWindow();
   return 0;
+}
+
+// Draw Grid Function (Eventually Removed)
+// Only for development, won't need visual grid for final game
+void drawGrid(int width, int height, int cellW, int cellH) {
+  // two For loops
+
+  // Vertical Lines
+  for (int i = 0; i < width; i += cellW) {
+    DrawLine(i + cellW, 0, i + cellW, height, GREEN);
+  }
+
+  // Horizontal Lines
+  for (int j = 0; j < height; j += cellH) {
+    DrawLine(0, j + cellH, width, j + cellH, GREEN);
+  }
 }
